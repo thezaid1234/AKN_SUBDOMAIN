a11176c9cd1b8ac261c951087d1a930865ef8627
thezaid1234 <156358813+thezaid1234@users.noreply.github.com>
2025-05-10 18:33:16 +0000

    Add files via upload

---
+++ SUBDOMAIN.py
@@ -0,0 +1,36 @@
+import requests
+
+https="https://"
+quote="""
+░█████╗░██╗░░██╗███╗░░██╗  ░██████╗██╗░░░██╗██████╗░██████╗░░█████╗░███╗░░░███╗░█████╗░██╗███╗░░██╗
+██╔══██╗██║░██╔╝████╗░██║  ██╔════╝██║░░░██║██╔══██╗██╔══██╗██╔══██╗████╗░████║██╔══██╗██║████╗░██║
+███████║█████═╝░██╔██╗██║  ╚█████╗░██║░░░██║██████╦╝██║░░██║██║░░██║██╔████╔██║███████║██║██╔██╗██║
+██╔══██║██╔═██╗░██║╚████║  ░╚═══██╗██║░░░██║██╔══██╗██║░░██║██║░░██║██║╚██╔╝██║██╔══██║██║██║╚████║
+██║░░██║██║░╚██╗██║░╚███║  ██████╔╝╚██████╔╝██████╦╝██████╔╝╚█████╔╝██║░╚═╝░██║██║░░██║██║██║░╚███║
+╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝  ╚═════╝░░╚═════╝░╚═════╝░╚═════╝░░╚════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝╚═╝░░╚══╝
+                                                            @Zaid barham
+                                                            Paypal:barhamzaid01@gmail.com
+                                                            """
+
+blue="\033[0;34m"
+yellow="\033[1;33m"
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+print(blue+quote)
+file=input(yellow+"Enter the subdomains path: ")
+
+with open(file, 'r') as file:
+    for line in file:
+        subdomain = line.strip()
+        if not subdomain:
+            continue
+        if "https://" in subdomain:
+            url = https + subdomain
+        else:
+            continue
+        url = subdomain
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            print(GREEN+f"[+] {url} is UP {response.status_code}")
+        else:
+            print(RED+f"[-] {url} responded with status code: {response.status_code}")
